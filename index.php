0c67418193cf89b52e107098652773c87388fc5c
Eric C. Weig <eweig@email.uky.edu>
2019-02-20 15:25:42 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+<title>Sample Sensitive Content Filter</title>
+</head>
+<body>
+<?php
+// https://stackoverflow.com/questions/15229732/show-a-div-onclick-and-hide-the-image-that-triggered-it/15229787
+
+$sensitiveText = "murder,dead,blood";
+$dctitle = "Crime scene photo of an unidentified murder victim.";
+
+$mySensitiveArray = explode(',', $sensitiveText);
+$needles = $mySensitiveArray;
+
+foreach ($needles as $needle)
+{
+    if (stripos($dctitle, $needle) !== FALSE)
+    {
+        $sensitiveFlag = "true";
+
+    } elseif (strpos($dcsource, $needle) !== FALSE) {
+
+        $sensitiveFlag = "true";
+    }
+}
+
+
+if ("true" == $sensitiveFlag):
+
+echo "<div><p id=\"clickSense\"><img src=\"sharp_sensitive.png\" onclick=\"show('sensitive_image'); hide('clickSense')\" alt=\"click me\" style=\"text-align:center\"></p>";
+
+
+echo "<div id=\"sensitive_image\" style=\"display:none;\"><img src=\"sensitive_image.jpg\" style=\width:480px;height:480px;\"/></div></div>";
+
+
+else:
+
+echo "<div><img src=\"sensitive_image.jpg\"/></div>";
+
+endif;
+
+?>
+<script>
+  function show (toBlock){
+    setDisplay(toBlock, 'block');
+  }
+  function hide (toNone) {
+    setDisplay(toNone, 'none');
+  }
+  function setDisplay (target, str) {
+    document.getElementById(target).style.display = str;
+  }
+</script>
+</html>
