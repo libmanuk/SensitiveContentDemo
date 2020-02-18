82c8cdbef652e71d47620b0379c3f078576bc1eb
Eric C. Weig <eweig@email.uky.edu>
2020-02-18 07:17:58 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,9 @@
 </head>
 <body>
     <div id="header"><h3>Sensitive Content Filter Demo</h3>
-    
     <h2 class="caption">Young Dewey Melvil (Great Pyrenees) sticking out his tongue.</h2></div>
+    
 <?php
-
 
 $sensitiveText = "tongue";
 $dctitle = "Young Dewey Melvil (Great Pyrenees) sticking out his tongue.";
@@ -28,14 +27,11 @@ foreach ($sensitives as $sensitive)
     }
 }
 
-
 if ("true" == $sensitiveFlag):
 
 echo "<div>\n<p id=\"clickSense\">\n<span id=\"tap_text\">click or tap to view image</span>\n<img src=\"sharp_sensitive.png\" onclick=\"show('sensitive_image'); hide('clickSense')\" alt=\"click me\" style=\"text-align:center\" class=\"center_simage\">\n</p>\n";
 
-
 echo "<div id=\"sensitive_image\" style=\"display:none;\">\n<img src=\"sensitive_image.jpg\" style=\width:480px;height:480px;\" class=\"center_image\"/>\n</div>\n</div>\n";
-
 
 else:
 
